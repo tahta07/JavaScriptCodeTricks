f143c78dd062be724d0cae3bb8fd72a86533236f
Yunus Erol <erol_yunus@hotmail.com>
2020-08-30 14:06:57 +0300

    this function checks for a valid email

---
+++ IsValidEmail/IsValidEmail.js
@@ -0,0 +1,27 @@
+function IsValidEmail(email) {
+        if (email.length <= 2) {
+            return false;
+        }
+
+        if (email.indexOf("@") == -1) {
+            return false;
+        }
+ 
+        var parts = email.split("@");
+        var dot = parts[1].indexOf(".");
+        var len = parts[1].length;
+        var dotSplits = parts[1].split(".");
+        var dotCount = dotSplits.length - 1;
+    
+        if (dot == -1 || dot < 2 || dotCount > 2) {
+            return false;
+        }
+
+        for (var i = 0; i < dotSplits.length; i++) {
+            if (dotSplits[i].length == 0) {
+                return false;
+            }
+        }
+ 
+        return true;
+    };
